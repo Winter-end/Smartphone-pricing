0f5df4a4100e040b75b10a650ca67c7b0adb31c8
Winter-end <persymon.n@gmail.com>
2024-05-31 10:30:13 +0200

    poprawa widoczności GUI

--- rest-api/app.py
+++ rest-api/app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
 from pymongo import MongoClient
+import pandas as pd
+import pickle
 
 app = Flask(__name__)
 db_client = MongoClient('localhost', 27017)
 
 # data = db_client.details
 # storage = data.storage
+#andorid_model = pickle.load(open("../exploratory-analysis/android_model.pkl", "rb"))
+#apple_model = pickle.load(open("../exploratory-analysis/apple_model.pkl", "rb"))
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -16,6 +21,7 @@ def index():
         storage = request.form.get('storage')
         # You can now use the form data
         print(f'OS: {os}, Ram: {ram}, Battery: {battery}, Storage: {storage}')
+        
     return render_template('index.html')
 
 if __name__ == '__main__':
