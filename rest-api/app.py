6416c462b37b017664c6864fb5262510210eb741
Winter-end <persymon.n@gmail.com>
2024-06-01 21:28:16 +0200

    dodanie pól tekstowych i obsługi w aplikacji

--- rest-api/app.py
+++ rest-api/app.py
@@ -16,11 +16,25 @@ db_client = MongoClient('localhost', 27017)
 def index():
     if request.method == 'POST':
         os = request.form.get('os')
+        front_camera = request.form.get('front_camera')
         ram = request.form.get('ram')
         battery = request.form.get('battery')
         storage = request.form.get('storage')
-        # You can now use the form data
-        print(f'OS: {os}, Ram: {ram}, Battery: {battery}, Storage: {storage}')
+        score = request.form.get('score')
+        scree_size = request.form.get('screen_size')
+        refresh_rate = request.form.get('refresh_rate')
+        main_camera = request.form.get('main_camera')
+
+        new_inputs = []
+        num_camera = len(request.form)
+        for i in range(2, num_camera):
+            new_input = request.form.get(f'new-input-{i}')
+            if new_input is not None:
+                new_inputs.append(new_input)
+
+        s = pd.Series([])
+
+        print(f'OS: {os}, Front camera: {front_camera}, Ram: {ram}, Battery: {battery}, Storage: {storage}, Score: {score}, Screen size: {scree_size}, Refresh rate: {refresh_rate}, Main camera: {main_camera}, New Inputs: {new_inputs}')
         
     return render_template('index.html')
 
